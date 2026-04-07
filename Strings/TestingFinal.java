8d143832c6b3113be4d54f49d171ca8f61a181e0
Aryan-pagar06 <aryanpagar34@gmail.com>
2026-04-07 22:36:07 +0530

    final keyword

---
+++ Strings/TestingFinal.java
@@ -0,0 +1,11 @@
+public class TestingFinal 
+{
+    final double pi = 3.1412; 
+    
+    void testing () {
+        pi = 56; // error cuz cannot assign new value to PI
+    }
+    public static void main(String[] args) {
+        
+    }
+}

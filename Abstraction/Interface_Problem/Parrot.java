a30ac9215916964f654621fc91e850a7762b5e0b
Aryan-pagar06 <aryanpagar34@gmail.com>
2026-05-05 16:34:31 +0530

    new stuff

---
+++ Abstraction/Interface_Problem/Parrot.java
@@ -0,0 +1,7 @@
+public class Parrot extends Bird 
+{
+    @Override
+    public void fly() {
+        System.out.println("Parrot is flying");
+    }
+}
---
+++ Abstraction/Interface_Problem/Test.java
@@ -0,0 +1,10 @@
+public class Test
+{
+    public static void main(String[] args) {
+        Eagle e = new Eagle();
+        e.fly();
+        Parrot p = new Parrot();
+        p.fly();
+
+    }
+}
